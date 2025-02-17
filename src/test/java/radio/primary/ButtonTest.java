043ecc196a64b040ef6ab961b713b1346c1436c2
Ponomarenko Alice <a.ponomarenko111@gmail.com>
2025-02-17 21:18:41 +0300

    Write module tests for Button

---
+++ src/test/java/radio/primary/ButtonTest.java
@@ -0,0 +1,62 @@
+package radio.primary;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import util.Observer;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class ButtonTest {
+    private Button button;
+
+    @BeforeEach
+    void setButton() {
+        MockitoAnnotations.openMocks(this);
+        button = new Button();
+    }
+
+    @Test
+    void testInitialPressState() {
+        assertFalse(button.isPressed(), "Should be not pressed initially");
+    }
+
+    @Test
+    void testPressChangeState() {
+        button.changePressState();
+        assertTrue(button.isPressed());
+
+        button.changePressState();
+        assertFalse(button.isPressed());
+    }
+
+    @Test
+    void testRegisterObserver() {
+        Observer testObserver = mock(Observer.class);
+        button.registerObserver(testObserver);
+        assertAll(() -> assertEquals(1, button.getObservers().size()),
+                () -> assertEquals(testObserver, button.getObservers().getFirst()));
+    }
+
+    @Test
+    void testRemoveObserver() {
+        Observer testObserver = mock(Observer.class);
+        button.registerObserver(testObserver);
+        button.removeObserver(testObserver);
+
+        assertEquals(0, button.getObservers().size());
+    }
+
+
+    @Test
+    void testNotifyObservers() {
+        Observer testObserver = mock(Observer.class);
+        button.registerObserver(testObserver);
+        button.changePressState();
+
+        verify(testObserver, times(1)).update();
+    }
+
+
+}
---
+++ src/test/java/radio/primary/RadioTest.java
@@ -0,0 +1,12 @@
+package radio.primary;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    @Test
+    void testButtonChangeState() {
+
+    }
+}
